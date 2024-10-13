6ea834ac11d873ad50e12a4c7b837ff5b1a9ba32
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-10-13 18:46:50 +0530

    update api

---
+++ api/models/User.jsx
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  // name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
+
+// const mongoose = require("mongoose");
+// const { Schema, model } = mongoose;
+// // user schema
+
+// const UserSchema = new Schema({
+//   name: { type: String, required: true },
+//   username: { type: String, required: true, min: 4, unique: true }, // validation
+//   password: { type: String, required: true },
+// });
+
+// // create model
+
+// const UserModel = model("User", UserSchema);
+
+// module.exports = UserModel;
